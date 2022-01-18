289e0dd13c509350031916058d2ebc41eeb8477b
jaseok75 <74055323+jaseok75@users.noreply.github.com>
2022-01-18 14:29:44 +0900

    202201181429 commit

---
+++ DongBright/DFS_BFS/연구소.py
@@ -0,0 +1,64 @@
+"""
+Project: 동빈나_DFS/BFS_연구소
+Date: 2022.01.17.월.
+"""
+
+import sys
+import itertools
+import copy
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def isRange(x, y):
+    if 0 <= x < n and 0 <= y < m:
+        return True
+    else:
+        return False
+
+def dfs(x, y):
+    visited[x][y] = False
+    new_map[x][y] = 2
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if isRange(nx, ny) and visited[nx][ny] and new_map[nx][ny] == 0:
+            dfs(nx, ny)
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    input_map = [list(map(int, input().split())) for _ in range(n)]
+    index_map = []
+    new_map = []
+    min_safety_area = n * m
+
+    for i in range(n):
+        for j in range(m):
+            if input_map[i][j] != 2 and input_map[i][j] != 1:
+                index_map.append((i, j))
+
+    beouk = list(itertools.combinations(index_map, 3))
+
+    for idx in beouk:
+        visited = [[True] * m] * n
+        safety_area = 0
+        new_map = copy.deepcopy(input_map)
+        new_map[idx[0][0]][idx[0][1]] = 1
+        new_map[idx[1][0]][idx[1][1]] = 1
+        new_map[idx[2][0]][idx[2][1]] = 1
+
+        for i in range(n):
+            for j in range(m):
+                if visited[i][j] and new_map[i][j] == 2:
+                    dfs(i, j)
+
+        for i in range(n):
+            for j in range(m):
+                if new_map[i][j] == 0:
+                    safety_area += 1
+        min_safety_area = min(safety_area, min_safety_area)
+
+    print(min_safety_area)
